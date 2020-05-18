546208ccf78d6a8880b91eab516caf6414a0ac23
Richard Willis <willis.rh@gmail.com>
2020-05-18 19:53:54 +0100

    Use custom dir to disable update
    Needs to be in short dir name, see https://github.com/microsoft/vscode/issues/86382

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,5 +1,7 @@
 import * as path from 'path';
 import * as cp from 'child_process';
+import * as fs from 'fs-extra';
+import * as os from 'os';
 
 import {
   runTests,
@@ -10,10 +12,19 @@ import {
 const VSCODE_VERSION = '1.45.0';
 
 async function main(): Promise<void> {
-  try {
-    const extensionDevelopmentPath = path.resolve(__dirname, '../..');
-    const extensionTestsPath = path.resolve(__dirname, './suite/index');
+  const extensionDevelopmentPath = path.resolve(__dirname, '../..');
+  const extensionTestsPath = path.resolve(__dirname, './suite/index');
+  const fixturePath = path.resolve(
+    __dirname,
+    '../../test-fixtures/gradle-project/'
+  );
+  const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'vscode-user'));
+  fs.copySync(
+    path.resolve(__dirname, '../../test-fixtures/vscode-user/User'),
+    path.join(tmpDir, 'User')
+  );
 
+  try {
     const vscodeExecutablePath = await downloadAndUnzipVSCode(VSCODE_VERSION);
     const cliPath = resolveCliPathFromVSCodeExecutablePath(
       vscodeExecutablePath
@@ -33,7 +44,7 @@ async function main(): Promise<void> {
       extensionDevelopmentPath,
       extensionTestsPath,
       launchArgs: [
-        path.resolve(__dirname, '../../test-fixtures/gradle-project/'),
+        fixturePath,
         '--disable-extension=vscjava.vscode-java-pack',
         '--disable-extension=redhat.java',
         '--disable-extension=vscjava.vscode-java-dependency',
@@ -42,11 +53,14 @@ async function main(): Promise<void> {
         '--disable-extension=eamodio.gitlens',
         '--disable-extension=sonarsource.sonarlint-vscode',
         '--disable-extension=esbenp.prettier-vscode',
+        `--user-data-dir=${tmpDir}`,
       ],
     });
   } catch (err) {
     console.error('Failed to run tests', err.message);
     process.exit(1);
+  } finally {
+    fs.removeSync(tmpDir);
   }
 }
 
