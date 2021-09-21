33985a3c2bb02e2f64a4e46f6338c161754bd1d0
forestaa <t.daichi.3@gmail.com>
2021-09-21 23:15:21 +0900

    Improve spotlessApply discovery for multi gradle project

--- src/Spotless.ts
+++ src/Spotless.ts
@@ -39,7 +39,7 @@ export class Spotless {
   }
 
   private hasSpotlessTask(tasks: vscode.Task[]): boolean {
-    return !!tasks.find((task) => task.name === 'spotlessApply');
+    return !!tasks.find((task) => task.name === 'spotlessApply' || task.name.endsWith(':spotlessApply'));
   }
 
   public async apply(
